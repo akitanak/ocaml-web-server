77f889c413762634b933e93d6c6b500dfc157dfc
akitanak <akihito_tanaka@me.com>
2018-11-19 23:53:09 +0900

    extract request handling routine

--- src/ows.ml
+++ src/ows.ml
@@ -6,76 +6,6 @@ let get_current_datetime () =
   Printer.Calendar.sprint "%a, %d %b %Y %H:%M:%S" (Calendar.now ())
 ;;
 
-type request_line =
-  { 
-    http_method: string;
-    request_target: string;
-    version: string;
-  }
-
-type header =
-  {
-    field_name: string;
-    field_value: string;
-  }
-
-(* type request_headers =
-  {
-    request_line: request_line;
-    headers: header list;
-  } *)
-
-let extract_request_line line =
-  match String.split line ~on:' ' with
-  | [http_method; request_target; version] ->  Some { http_method; request_target; version }
-  | _ -> None
-
-let read_request_line r =
-  Reader.read_line r
-  >>| function
-  | `Eof -> None
-  | `Ok line -> extract_request_line line
-
-let extract_header line =
-  match String.lsplit2 line ~on:':' with
-  | Some (name, value) -> Some { field_name = String.strip name; field_value = String.strip value }
-  | _ -> None
-
-let rec read_headers acc r =
-  Reader.read_line r
-  >>= function
-  | `Eof -> return acc
-  | `Ok line -> 
-    if (String.length line) = 0 then return acc
-    else
-      match extract_header line with
-      | Some header -> read_headers (header :: acc) r
-      | _ -> return acc
-
-let read_content r headers =
-  let exist_content_len h = h.field_name = "Content-Length" in
-  match List.find headers ~f:exist_content_len with
-  | None -> return None
-  | Some content_length ->
-    match int_of_string_opt content_length.field_value with
-    | None -> return None
-    | Some length ->
-      let buffer = Bytes.create length in
-      Reader.read r ~len:length buffer
-      >>|function
-      | `Eof -> None
-      | `Ok _ -> Some buffer
-
-
-let read_request r =
-  read_request_line r
-  >>= fun maybe_status_line ->
-  read_headers [] r
-  >>= fun headers ->
-  read_content r headers
-  >>| fun maybe_content ->
-  (maybe_status_line, headers, maybe_content)
-
 let response_headers () = [
   ("Content-Type", "text/html");
   ("Content-Length", "48")
@@ -93,16 +23,13 @@ let run ()=
       ~on_handler_error: `Raise
       (Tcp.Where_to_listen.of_port 8765)
       (fun _addr r w ->
-        read_request r
+        Request.read r
         >>= function
-        | (None, _, _) ->
-          Writer.write w "HTTP/1.1 400 Bad Request\n";
-          Writer.flushed w
-        | (Some request_line, _, Some content) ->
+        | (request_line, _, Some content) ->
           print_string (Bytes.to_string content);
           Writer.write w (request_line.version ^ " " ^ "200" ^ " " ^ "OK"  ^"\n");
           Writer.flushed w
-        | (Some request_line, _, None) ->
+        | (request_line, _, None) ->
           Writer.write w (request_line.version ^ " " ^ "200" ^ " " ^ "OK"  ^"\n");
           Writer.flushed w
         >>= fun () ->
---
+++ src/request.ml
@@ -0,0 +1,67 @@
+open Core;;
+open Async;;
+
+type request_line =
+  { 
+    http_method: string;
+    request_target: string;
+    version: string;
+  }
+
+type header =
+  {
+    field_name: string;
+    field_value: string;
+  }
+
+exception Invalid_status_line of string;;
+
+let extract_request_line line =
+  match String.split line ~on:' ' with
+  | [http_method; request_target; version] -> { http_method; request_target; version }
+  | _ -> raise (Invalid_status_line "status line must be consists http method, request target, http version separated with space.")
+
+let read_request_line r =
+  Reader.read_line r
+  >>| function
+  | `Eof -> raise (Invalid_status_line "No status line")
+  | `Ok line -> extract_request_line line
+
+let extract_header line =
+  match String.lsplit2 line ~on:':' with
+  | Some (name, value) -> Some { field_name = String.strip name; field_value = String.strip value }
+  | _ -> None
+
+let rec read_headers acc r =
+  Reader.read_line r
+  >>= function
+  | `Eof -> return acc
+  | `Ok line -> 
+    if (String.length line) = 0 then return acc
+    else
+      match extract_header line with
+      | Some header -> read_headers (header :: acc) r
+      | _ -> return acc
+
+let read_content r headers =
+  let exist_content_len h = h.field_name = "Content-Length" in
+  match List.find headers ~f:exist_content_len with
+  | None -> return None
+  | Some content_length ->
+    match int_of_string_opt content_length.field_value with
+    | None -> return None
+    | Some length ->
+      let buffer = Bytes.create length in
+      Reader.read r ~len:length buffer
+      >>|function
+      | `Eof -> None
+      | `Ok _ -> Some buffer
+
+let read r =
+  read_request_line r
+  >>= fun status_line ->
+  read_headers [] r
+  >>= fun headers ->
+  read_content r headers
+  >>| fun maybe_content ->
+  (status_line, headers, maybe_content)
